9c3750061112559c984469343459881aaa27f7a4
Mikel Martinez <63254253+mkbaraka@users.noreply.github.com>
2024-05-02 23:24:17 +0200

    Script para MQTT

---
+++ ACMSmart_Interface_MQTT/wifi_mqtt.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_MQTT_H
+#define WIFI_MQTT_H
+
+
+const char* ssid = "***"; // WiFi Name
+const char* password = "***";  // WiFi Password
+const char* mqtt_server = "192.168.1.15"; // MQTT broker IP
+
+
+#endif
